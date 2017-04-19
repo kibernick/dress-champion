bc73d3abab20aad44d7c68bf75ea1df3af4adecb
Nikola Rankovic <kibernick@gmail.com>
2017-04-19 23:21:03 +0200

    fix bugs with concurrent updates

--- dress_champion/models.py
+++ dress_champion/models.py
@@ -15,6 +15,7 @@ log = logging.getLogger(__name__)
 
 
 class Base(db.Model):
+    """Abstract base class for our models."""
     __abstract__ = True
 
     created_on = Column(DateTime, default=db.func.utc_timestamp())
@@ -23,24 +24,29 @@ class Base(db.Model):
     @classmethod
     def get_or_create(cls, pk_name, pk_value):
         """A get_or_create method that is uses DB savepoints to avoid race conditions. Will create a new
-        object if one does not exist, otherwise returns the existing object by this primary key value."""
+        object if one does not exist, otherwise returns the existing object by this primary key value.
+        
+        Returns:
+            object: Instance of the model queried/created.
+            bool: Was a new instance created or not.
+        
+        """
         obj = cls.query.get(pk_value)
         if obj is not None:
-            return obj
+            return obj, False
         obj = cls()
         setattr(obj, pk_name, pk_value)
-        # Create a savepoint in case of race condition.
-        db.session.begin_nested()
         try:
             db.session.add(obj)
-            # Try to commit and release the savepoint.
             db.session.commit()
-        except IntegrityError as e:
-            # The insert failed due to a concurrent transaction.
+            return obj, True
+        except Exception as e:
+            log.warning('The insert failed due to a concurrent transaction: {}'.format(pk_value))
             db.session.rollback()
             # Get the latest object with this pk that exists now.
             obj = cls.query.get(pk_value)
-        return obj
+            assert obj is not None, "Error retrieving <{}: {}>".format(cls.__name__, pk_value)
+            return cls.query.get(pk_value), False
 
 
 def coerce_to_utc(datetime_str):
@@ -73,36 +79,39 @@ class Dress(Base):
 
     @classmethod
     def consume_dresses_payload(cls, msg):
-        # todo: update, NOT create!
+        # todo: locking
         payload = msg.get('payload')
         if not payload:
             log.warning('Missing payload.')
             return
 
-        dress = cls.get_or_create('uid', payload['id'])
-
-        # todo: locking
-        dress.activation_date = coerce_to_utc(payload['activation_date'])
-        dress.name = payload['name']
-        dress.season = payload['season']
-        dress.price = Decimal(payload['price'])
-        dress.images = payload['images']
-        dress.brand = payload['brand']
+        dress, _ = cls.get_or_create('uid', payload['id'])
+        updates = {
+            'activation_date': coerce_to_utc(payload['activation_date']),
+            'name': payload.get('name'),
+            'season': payload.get('season'),
+            'price': Decimal(payload['price']) if payload.get('price') else None,
+            'images': payload.get('images'),
+            'brand': payload.get('brand'),
+        }
+        for attr, value in updates.items():
+            if value is not None:
+                setattr(dress, attr, value)
 
         db.session.add(dress)
         db.session.commit()
 
     @classmethod
     def consume_ratings_payload(cls, msg):
+        # todo: locking
         payload = msg.get('payload')
         if not payload:
             log.warning('Missing payload.')
             return
 
         dress_uid, rating = payload['dress_id'], payload['stars']
-        dress = cls.get_or_create('uid', payload['dress_id'])
+        dress, _ = cls.get_or_create('uid', payload['dress_id'])
 
-        # todo: locking
         ratings = dress.ratings if dress.ratings else []
         ratings.append(rating)
         dress.ratings = ratings
